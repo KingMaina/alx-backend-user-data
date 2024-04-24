3f2c47ce07bd39fba11699118bf31b2129c01df5
Andrew <drew29799@gmail.com>
2024-04-25 00:19:18 +0300

    Fix error exception

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -8,7 +8,7 @@ from db import DB
 from user import User
 
 
-def _hash_password(password: str) -> str:
+def _hash_password(password: str) -> bytes:
     """Encrypts a password"""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
@@ -29,10 +29,11 @@ class Auth:
         """Registers a new user"""
         try:
             self._db.find_user_by(email=email)
-            raise ValueError("User {} already exists".format(email))
         except NoResultFound:
             hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)
+        else:
+            raise ValueError("User {} already exists".format(email))
 
     def valid_login(self, email: str, password: str) -> bool:
         """Validates user credentials"""
