e733e02a4ec894cba2992bf7089a59bb707b676f
Andrew <drew29799@gmail.com>
2024-04-26 01:45:25 +0300

    Add: catch exception when searching for user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -55,10 +55,11 @@ class Auth:
         """Get the user from a session"""
         if session_id is None:
             return None
-        user = self._db.find_user_by(session_id=session_id)
-        if user is None:
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
             return None
-        return user
 
     def destroy_session(self, user_id: str) -> None:
         """Destroys a user session"""
